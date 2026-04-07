a420bfa3d7c798fe9f176a61ff1511993a62e297
Gdankz <gabrielbayu03@gmail.com>
2026-04-07 15:02:03 +0700

    Update Profile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,33 @@
+import { ArrowDown } from "lucide-react"
 
 
 export const HeroSection = () => {
-    return <section></section>
+    return <section id="hero"
+        className="relative min-h-screen flex flex-col items-center justify-center px-4"
+    >
+        <div className="container max-w-4xl mx-auto text-center z-10">
+            <div className="space-y-6">
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                    <span className="opacity-0 animate-fade-in"> Hi, I'm </span>
+                    <span className="text-primary opacity-0 animate-fade-in-delay-1"> Gabriel </span>
+                    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-1"> Bayu </span>
+                </h1>
+                <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    Innovative engineer specializing in the convergence of hardware and software, with deep expertise in Raspberry Pi, Arduino, Machine Learning, and Backend development. I focus on architecting end-to-end intelligent solutions, seamlessly integrating smart device prototyping with robust,
+                    scalable server-side infrastructures to build high-performance, data-driven ecosystems.
+                </p>
+                <div className=" pt-4 opacity-0 animate-fade-in-delay-4">
+                    <a href="#projects" className="cosmic-button">
+                        View My Work
+                    </a>
+                </div>
+            </div>
+
+            <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+                <span className="text-sm text-muted-foreground mb-2"> Scroll </span>
+                <ArrowDown className="h-5 w-5 text-primary" />
+            </div>
+
+        </div>
+    </section>
 }
