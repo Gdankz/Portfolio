2d242eda7ce73165a6900a1d412adb0e0e6fb1c2
Gabriel Bayu Handriyandanar <gabrielbayu03@gmail.com>
2026-08-30 15:43:43 +0700

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/Portfolio/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
